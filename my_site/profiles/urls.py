085392248fa347b74a9967dd1004f1f464f86afc
Sofiia Kaparnyk <s.kaparnyk@levi9.com>
2023-07-09 12:08:28 +0300

    feat(*): add static files serving

--- my_site/profiles/urls.py
+++ my_site/profiles/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import ProfilesView
 
-urlpatterns = [path("", views.CreateProfileView.as_view())]
+urlpatterns = [
+    path("", views.CreateProfileView.as_view()),
+    path("user-profiles/", ProfilesView.as_view()),
+]
--- my_site/profiles/views.py
+++ my_site/profiles/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import redirect, render
-from django.views import View
+from django.views.generic import CreateView, ListView
 
-from profiles.forms import FileUploadForm
 from profiles.models import UserProfile
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = FileUploadForm()
-        return render(request, "profiles/create_profile.html", {"form": form})
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    success_url = "/profiles/"
+    fields = ["image"]
 
-    def post(self, request):
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            file = request.FILES["image"]
-            UserProfile.objects.create(image=file)
-            return redirect("/profiles")
-        return render(request, "profiles/create_profile.html", {"form": form})
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
+
+
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = FileUploadForm()
+#         return render(request, "profiles/create_profile.html", {"form": form})
+#
+#     def post(self, request):
+#         form = FileUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             file = request.FILES["image"]
+#             UserProfile.objects.create(image=file)
+#             return redirect("/profiles")
+#         return render(request, "profiles/create_profile.html", {"form": form})
